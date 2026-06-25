613f40ee9dffa2654e90c819475a4b292efdec63
Anvesha <anvi5765sha@gmail.com>
2026-06-25 23:59:28 +0530

    Remove obsolete components and pages

--- client/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="bg-gray-900 text-white text-center py-6 mt-10">
-      <p>Copyright 2026 AgriAI. All Rights Reserved.</p>
-    </footer>
-  );
-}
-
-export default Footer;
